80d5e3e51869f5e76da8f127bc66de7a9d2f32a8
sumith <sumith95023@gmail.com>
2024-06-15 21:59:40 +0530

    updated

---
+++ src/components/footer/footer.jsx
@@ -0,0 +1,81 @@
+import React from 'react'
+import './footer.scss'
+import fb from "../assets/facebook.png";
+import ig from "../assets/instagram.png";
+import tw from "../assets/twitter.png";
+import In from "../assets/linkedin.png";
+
+function Footer() {
+  return (
+    <div className="footer">
+        <div className='sb__footer section__padding'>
+            <div className='sb__footer-links'>
+                <div className='sb__footer-links_div'>
+                    <h4>For Business</h4>
+                    <a href='/emplouer'>
+                        <p>Employer</p>
+                    </a>
+                    <a href='/emplouer'>
+                        <p>Customer App</p>
+                    </a> 
+                    <a href='/emplouer'>
+                        <p>Digital Inventory</p>
+                    </a>
+                </div>
+                <div className='sb__footer-links_div'>
+                    <h4>Partners</h4>
+                    <a href='/emplouer'>
+                        <p>Swing Tech</p>
+                    </a>
+                
+                </div>
+                <div className='sb__footer-links_div'>
+                    <h4>Company</h4>
+                    <a href='/emplouer'>
+                        <p>About</p>
+                    </a>
+                    <a href='/emplouer'>
+                        <p>Press</p>
+                    </a> 
+                    <a href='/emplouer'>
+                        <p>Career</p>
+                    </a>
+                    <a href='/emplouer'>
+                        <p>Contact</p>
+                    </a>
+                </div>
+                <div className='sb__footer-links_div'>
+                    <h4>Coming soon on</h4>
+                    <div className="socialmedia">
+                        <p><img src={fb} alt="fb"/></p>
+                        <p><img src={In} alt="fb"/></p>
+                        <p><img src={tw} alt="fb"/></p>
+                        <p><img src={ig} alt="fb"/></p>
+
+                    </div>
+                   
+                </div>
+            </div>
+
+            <hr></hr>
+
+            <div className='sb__footer-below'>
+                <div className='sb__footer-copyright'>
+                    <p>
+                        @{new Date().getFullYear()} Grig Technologies Pvt Ltd
+                    </p>
+                </div>
+                <div className="sb__footer-below-links">
+                    <a href='/terms'><div><p>Terms & Conditions</p></div></a>
+                    <a href='/terms'><div><p>Privacy & policy</p></div></a>
+                    <a href='/terms'><div><p>Return & refund policy</p></div></a>
+                    <a href='/terms'><div><p>Plans and pricing</p></div></a>
+
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Footer
---
+++ src/components/navbar/navbar.jsx
@@ -0,0 +1,84 @@
+import React, { useState } from "react";
+import './navbar.scss';
+import { RiMenu3Line, RiCloseLine } from 'react-icons/ri';
+import logo from '../assets/full logo.png'; // Make sure the path to the logo is correct
+
+const Navbar = () => {
+    const [toggleMenu, setToggleMenu] = useState(false);
+    return (
+        <div className="navbar-bg">
+            <div className="sb__navbar">
+                <div className="sb__navbar-links">
+                    <div className="sb__navbar-links_logo">
+                        <a href="www.google.com">
+                            <img src={logo} alt="logo" />
+                        </a>
+                    </div>
+                    <div className="sb__navbar-links_container">
+                        <p>
+                            <a href="www.google.com">
+                                Our partners
+                            </a>
+                        </p>
+                        <p>
+                            <a href="www.google.com">
+                                Plans and pricing
+                            </a>
+                        </p>
+                        <p>
+                            <a href="www.google.com">
+                                How to use?
+                            </a>
+                        </p>
+                        <p>
+                            <a href="www.google.com">
+                                Features
+                            </a>
+                        </p>
+                    </div>
+                </div>
+                <div className="sb__navbar-button">
+                    <a href="www.google.com">
+                        <button type="button">LOGIN</button>
+                    </a>
+                </div>
+                <div className="sb__navbar-menu">
+                    {toggleMenu ? (
+                        <RiCloseLine
+                            color="#000"
+                            size={27}
+                            onClick={() => setToggleMenu(false)}
+                        />
+                    ) : (
+                        <RiMenu3Line
+                            color="#000"
+                            size={27}
+                            onClick={() => setToggleMenu(true)}
+                        />
+                    )}
+                    {toggleMenu && (
+                        <div className="sb__navbar-menu_container scale-up-center">
+                            <div className="sb__navbar-menu_container-links">
+                                <p>
+                                    <a href="www.google.com">Our partners</a>
+                                </p>
+                                <p>
+                                    <a href="www.google.com">Plans and pricing</a>
+                                </p>
+                                <p>
+                                    <a href="www.google.com">How to use?</a>
+                                </p>
+                                <p>
+                                    <a href="www.google.com">Features</a>
+                                </p>
+                            </div>
+                           
+                        </div>
+                    )}
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Navbar;
